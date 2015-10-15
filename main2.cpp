c164c6acc005950f629149e27ff0f1501cd34d1c
vvw <siluplsy@gmail.com>
2015-10-15 17:38:58 +0800

    call imread

--- main2.cpp
+++ main2.cpp
@@ -1,10 +1,6 @@
 
 #include "stdio.h"
-//#include "mcadd.h"
 #include "libmcadd.h"
-
-//#pragma comment(lib, "../bin/matlib.lib")
-//#pragma comment(lib, "mcadd.lib")
 
 #pragma comment(lib, "mclmcr.lib")
 #pragma comment(lib, "mclmcrrt.lib")
@@ -12,23 +8,21 @@
 
 // G:\Program Files\MATLAB\R2014a\extern\include
 
-//函数的返回是变相的放到形参中的，依次是输出参数个数，输出参数，输入参数。
-//extern LIB_my_CPP_API void MW_CALL_CONV mcadd(int nargout, mwArray& y, const mwArray& u, const mwArray& v);
-
-//extern "C" int mcadd(int, int);
-
-//int add(int a, int b);
+// 函数的返回是变相的放到形参中的，依次是输出参数个数，输出参数，输入参数。
+// mcadd 内部调用imread 和size 读取当前止录下的 head.jpg ，返回宽和高
+// void MW_CALL_CONV mcadd(int nargout, mwArray& w, mwArray& h, const mwArray& u, const mwArray& v);
 
 /*
 mcadd.m
-function y = mcadd(u, v) % #codegen
-%image = imread('head.jpg');
-%[rows, cols] = size(image);
-%y = rows;
-y = 7;
+function [w, h] = mcadd (u, v) %#codegen
+image = imread('head.jpg');
+[rows, cols] = size(image);
+w = cols;
+h = rows;
 end
 
 % mcc -W cpplib:libmcadd -T link:lib mcadd.m
+% 以上命令直接在matlab 的命令窗口执行
 */
 
 int main() {
@@ -38,7 +32,8 @@ int main() {
 	int a = 3;
 	int b = 4;
 
-	mwArray y(1, 1, mxINT32_CLASS);
+	mwArray w(1, 1, mxINT32_CLASS);
+	mwArray h(1, 1, mxINT32_CLASS);
 	mwArray u(1, 1, mxINT32_CLASS);
 	mwArray v(1, 1, mxINT32_CLASS);
 
@@ -48,12 +43,14 @@ int main() {
 	u (1, 1) = 3;
 	v (1, 1) = 4;
 
-	mcadd(1, y, u, v);
+	mcadd(2, w, h, u, v);
 
-	int p;
-	y.GetData(&p, 1);
+	int p1;
+	int p2;
+	w.GetData(&p1, 1);
+	h.GetData(&p2, 1);
 
-	printf("ok? %d\nresults: %d", successQ, p);
+	printf("ok? %d\nimage's width,hight = %d,%d\n", successQ, p1, p2);
 
 	getchar();
 	return 0;
